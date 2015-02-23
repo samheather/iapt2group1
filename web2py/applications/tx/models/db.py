557ba412f4701c0e8fc40c0c7cba552deef60423
Sam Heather <sam@heather.sh>
2015-02-23 20:07:18 +0000

    Updated physical data model in web2py and resolved variable names in umlet.

--- web2py/applications/tx/models/db.py
+++ web2py/applications/tx/models/db.py
@@ -8,19 +8,20 @@ auth = Auth(db)
 auth.define_tables(username=True,signature=False)
 
 db.define_table('TranscriptionFieldType',
-			Field('type', 'string', requires=IS_LENGTH(minsize=1, maxsize=100), required=True)
+			Field('type', 'string', requires=IS_LENGTH(minsize=1, maxsize=100), required=True),
+			Field('friendlyName', 'string', requires=IS_LENGTH(minsize=1, maxsize=60), required=True)
 )
 
 if db(db.TranscriptionFieldType.id > 0).count() == 0:
 	db.TranscriptionFieldType.bulk_insert([
-						{'type':'textfield'},
-						{'type':'textarea'},
-						{'type':'date'}])
+						{'type':'textfield','friendlyName':'Text Field'},
+						{'type':'textarea','friendlyName':'Multi-lined Text'},
+						{'type':'date','friendlyName':'Date Selector'}])
 	db.commit()
 
 db.define_table('Project',
 			Field('title', 'string', label='Title', requires=IS_LENGTH(minsize=1, maxsize=100), required=True),
-			Field('requestDescription', 'string', label='Request Description', requires=IS_LENGTH(minsize=1, maxsize=100), required=True), \
+			Field('requestDescription', 'string', label='Request Description', requires=IS_LENGTH(minsize=1, maxsize=100), required=True),
 			Field('ownerId', db.auth_user, required=True),
 			Field('projectOpen', 'boolean', required=True)
 )
@@ -32,24 +33,24 @@ db.define_table('Image',
 			Field('imageDescription', 'string', label='Image Description', requires=[IS_LENGTH(minsize=0,error_message="Please enter an image description"),IS_LENGTH(minsize=0, maxsize=100,error_message="Please enter an image description shorter than 100 characters")], required=True)
 )
 
-db.define_table('TranscriptionObject',
+db.define_table('Transcription',
 			Field('image_id', db.Image, required=True),
-			Field('owner_id', db.auth_user, required=True),
+			Field('transcriber_id', db.auth_user, required=True),
 			Field('rejected', 'boolean', required=True)
 )
 
 """
 Accepted transcription is nullable as the default transcription for a project is none
 """
-db.Image.acceptedTranscriptionObject_id = Field('acceptedTranscriptionObject_id', db.TranscriptionObject, required=False)
+db.Image.acceptedTranscription_id = Field('acceptedTranscription_id', db.Transcription, required=False)
 
 db.define_table('TranscriptionField',
-			Field('type_id', db.TranscriptionFieldType, required=True),
-			Field('transcriptionObject_id', db.TranscriptionObject, required=True),
+			Field('projectField_id', db.TranscriptionFieldType, required=True),
+			Field('transcription_id', db.Transcription, required=True),
 			Field('value', 'string', required=True)
 )
 
-db.define_table('FieldsForProject',
+db.define_table('ProjectField',
 			Field('project_id', db.Project, required=True),
 			Field('type_id', db.TranscriptionFieldType, required=True),
 			Field('label', 'string', requires=IS_LENGTH(minsize=1, maxsize=20), required=True)
