c0b3185c369a976a4511688e9cde0b4b6c22365d
Sam Heather <sam@heather.sh>
2015-02-25 00:33:36 +0000

    Started adding addImage etc

--- web2py/applications/tx/controllers/project.py
+++ web2py/applications/tx/controllers/project.py
@@ -8,8 +8,20 @@ def create():
 	form = BOOTUPFORM(db.Project)
 	message = ""
 	if form.process().accepted:
-		redirect(URL('project', 'populate'))
+		redirect(URL('project', 'populate', args=form.vars.id))
 	return dict(form=form, message=message)
-    
+	
 def populate():
-	return dict()
+	project_id = request.args(0)
+	project = db((db.Project.id == project_id)).select()[0]
+	return dict(project=project)
+	
+def addImage():
+	project_id = request.args(0)
+	form = BOOTUPFORM(db.Image)
+	form.vars.project_id = project_id
+	db.Image.project_id.readable = db.Image.project_id.writable = False
+	message = ""
+	if form.process().accepted:
+		redirect(URL('project', 'populate', args=project_id))
+	return dict(form=form, message=message)
--- web2py/applications/tx/models/db.py
+++ web2py/applications/tx/models/db.py
@@ -22,32 +22,16 @@ if db(db.TranscriptionFieldType.id > 0).count() == 0:
 db.define_table('Project',
 			Field('title', 'string', label='Title', requires=IS_LENGTH(minsize=1, maxsize=100), required=True),
 			Field('requestDescription', 'string', label='Request Description', requires=IS_LENGTH(minsize=1, maxsize=100), required=True),
-			Field('ownerId', db.auth_user, required=True),
+			Field('owner_id', db.auth_user, required=True),
 			Field('projectOpen', 'boolean', required=True)
 )
 
 ## TODO - we need to explain our 100 char image description limit below.
 db.define_table('Image',
-			Field('projectId', db.Project, required=True),
+			Field('project_id', db.Project, required=True),
 			Field('image', 'upload'),
-			Field('imageDescription', 'string',
-                  label='Image Description',
-                  requires=[
-                      IS_LENGTH(minsize=0,error_message="Please enter an image description"),
-                      IS_LENGTH(minsize=0, maxsize=100,error_message="Please enter an image description shorter than 100 characters")],
-                  required=True
-                  ),
-            migrate=False
+			Field('imageDescription', 'string', label='Image Description', requires=[IS_LENGTH(minsize=0,error_message="Please enter an image description"),IS_LENGTH(minsize=0, maxsize=100,error_message="Please enter an image description shorter than 100 characters")], required=True)
 )
-
-
-db.executesql('CREATE TABLE IF NOT EXISTS Image '
-              '(id              INTEGER PRIMARY KEY AUTOINCREMENT,'
-              'project_id        INTEGER REFERENCES Project (id) ON DELETE CASCADE,'
-              'image            CHAR(512),'
-              'imageDescription CHAR(512),'
-              'acceptedTranscription_id INTEGER REFERENCES Image(id) ON DELETE SET NULL)')
-
 
 db.define_table('Transcription',
 			Field('image_id', db.Image, required=True),
@@ -73,33 +57,10 @@ db.define_table('ProjectField',
 )
 
 
-db.executesql('CREATE VIEW IF NOT EXISTS ImagesForTranscription AS'
-              ' SELECT *, COUNT(Transcription.id) AS transcriptionCount'
-              ' FROM Image'
-              ' LEFT JOIN Transcription ON Transcription.image_id=Image.id'
-              ' WHERE Image.acceptedTranscription_id IS NULL'
-              '     AND Transcription.rejected IS NOT 1'
-              ' GROUP BY Image.id'
-              ' HAVING COUNT(Transcription.id)<3')
 
 
 
-db.executesql('CREATE VIEW IF NOT EXISTS ProjectsForTranscription AS'
-              ' SELECT *, COUNT(ImagesForTranscription.id) AS imageCount'
-              ' FROM Project'
-              ' LEFT JOIN ImagesForTranscription ON '
-              '     ImagesForTranscription.project_id = Project.Id'
-              ' WHERE project.projectOpen'
-              ' GROUP BY project.id'
-              ' HAVING count(ImagesForTranscription.id)>0')
 
-
-
-db.define_table('ProjectsForTranscription',
-                Field('project_id'),
-                Field('image'),
-                Field('image_description'),
-                Field('imageCount'),migrate=False)
 
 
 
