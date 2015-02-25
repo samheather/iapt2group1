9c1e47ec977eea5e503d86050bce9abde81a862a
Sam Heather <sam@heather.sh>
2015-02-25 01:18:07 +0000

    Added views with basic forms for adding project, updated the data model for the Field -> ProjectField

--- web2py/applications/tx/controllers/project.py
+++ web2py/applications/tx/controllers/project.py
@@ -18,10 +18,24 @@ def populate():
 	
 def addImage():
 	project_id = request.args(0)
-	form = BOOTUPFORM(db.Image)
-	form.vars.project_id = project_id
+	# Hide project ID Field
 	db.Image.project_id.readable = db.Image.project_id.writable = False
+	form = BOOTUPFORM(db.Image)
+	# Fill in the Project ID
+	form.vars.project_id = project_id
 	message = ""
 	if form.process().accepted:
 		redirect(URL('project', 'populate', args=project_id))
 	return dict(form=form, message=message)
+	
+def addField():
+	project_id = request.args(0)
+	# Hide project ID Field
+	db.ProjectField.project_id.readable = db.ProjectField.project_id.writable = False
+	form = BOOTUPFORM(db.ProjectField)
+	# Fill in the Project ID
+	form.vars.project_id = project_id
+	message = ""
+	if form.process().accepted:
+		redirect(URL('project', 'populate', args=project_id))
+	return dict(form=form, message=message)	
